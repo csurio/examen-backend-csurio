486a2c1147c7f85e6315f66681980dc9fa5bc0cd
Carlos Surio <alfredo_monterrosa@siman.com>
2025-04-01 10:46:21 -0600

    feat: add transaction controller

---
+++ src/main/java/com/assessment/bank/presentation/controller/TransactionController.java
@@ -0,0 +1,15 @@
+package com.assessment.bank.presentation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Carlos M. Surio
+ * @since  1 abr 2025
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+
+}
